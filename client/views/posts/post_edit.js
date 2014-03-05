2a9b0d57567fcd1747ffacd6da60ed7a8f2170ad
Emanuele 'Lele' Calo <lele@amicofigo.com>
2014-03-05 03:38:56 +0100

    Fixed issue on edit page

--- client/views/posts/post_edit.js
+++ client/views/posts/post_edit.js
@@ -1,6 +1,6 @@
 Template.postEdit.helpers({
   post: function() {
-    return Posts.findOne(Session.get('currentPostId'));
+    return Posts.findOne({id: Session.get('currentPostId')});
   }
 });
 Template.postEdit.events({
